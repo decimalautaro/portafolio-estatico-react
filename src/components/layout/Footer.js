e078b3c6edaae30cbb2257c69d700e15aa180006
Lautaro Decima <decimalautaro@gmail.com>
2023-02-05 05:00:02 -0300

    feat:creando componentes

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer>
+        Portafolio web Lautaro Decima &copy; Desarrollado con React 2023
+    </footer>
+  )
+}
---
+++ src/components/layout/HeaderNav.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export const HeaderNav = () => {
+  return (
+    <header className='header'>
+      <div className='logo'>
+        <span>L</span>
+        <h3>Lautaro Decima</h3>
+      </div>
+        <nav>
+          <ul>
+            <li>
+                <NavLink to="home">Inicio</NavLink>
+            </li>
+            <li>
+                <NavLink to="portafolio">Portafolio</NavLink>
+            </li>
+            <li>
+                <NavLink to="services">Servicios</NavLink>
+            </li>
+            <li>
+                <NavLink to="curriculum">Curriculum</NavLink>
+            </li>
+            <li>
+                <NavLink to="contact">Contacto</NavLink>
+            </li>
+          </ul>
+        </nav>
+    </header>
+  )
+}
