449a58573887a9dedebde0718f5564c682dbdae2
Lautaro Decima <decimalautaro@gmail.com>
2023-02-05 05:00:59 -0300

    feat: configurando rutas

---
+++ src/router/MyRoutes.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {Routes, Route, BrowserRouter, NavLink} from "react-router-dom"
+import {HomePage} from "../components/HomePage"
+import {Portafolio} from "../components/Portafolio"
+import {Services} from "../components/Services"
+import {Curriculum} from "../components/Curriculum"
+import {Contact} from "../components/Contact"
+import { HeaderNav } from '../components/layout/HeaderNav'
+import { Footer } from '../components/layout/Footer'
+
+export const MyRoutes = () => {
+  return (
+    <BrowserRouter>
+    {/* HEADER Y NAVEGACION */}
+      <HeaderNav/>
+
+    {/* CONTENIDO CENTRAL */}
+    <Routes>
+        <Route path='/' element= {<HomePage/>}>Inicio</Route>
+        <Route path='/home' element= {<HomePage/>}>Inicio</Route>
+        <Route path='/portafolio' element= {<Portafolio/>}>Portafolio</Route>
+        <Route path='/services' element= {<Services/>}>Servicio</Route>
+        <Route path='/curriculum' element= {<Curriculum/>}>Curriculum</Route>
+        <Route path='/contact' element= {<Contact/>}>Contacto</Route>
+        
+
+    </Routes>
+
+      <hr/>
+    {/* FOOTER */}
+      <Footer/>
+   
+    
+    </BrowserRouter>
+  )
+}
