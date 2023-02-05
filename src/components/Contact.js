26d948c77284d233533d1235649d28d40f9ddb4f
Lautaro Decima <decimalautaro@gmail.com>
2023-02-05 16:18:07 -0300

    feat: haciendo formulario de contacto

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
+
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='page'>
+      <h1>Contacto</h1>
+
+      <form className='contact' action='mailto:decimalautaro@gmail.com'>
+          <input type="text" placeholder='Nombre'/>
+          
+          <input type="text" placeholder='Apellido'/>
+          
+          <input type="email" placeholder='Email'/>
+          
+          <textarea placeholder='Motivo de contacto'/>
+
+          <input type="submit" value="Enviar"/>
+
+      </form>
+    </div>
   )
 }
