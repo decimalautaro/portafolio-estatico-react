1bff45c133372c272ef8d763fc27f292047a825b
Lautaro Decima <decimalautaro@gmail.com>
2023-02-05 16:02:08 -0300

    feat: llenando datos de cv

--- src/components/Curriculum.js
+++ src/components/Curriculum.js
@@ -2,6 +2,75 @@ import React from 'react'
 
 export const Curriculum = () => {
   return (
-    <div>Curriculum</div>
+    <div className='page'>
+    <h1>
+      Curriculum Vitae
+    </h1>
+
+    <h2>Experiencia laboral:</h2>
+
+      <ul>
+        <li>Soporte Tecnico y mantenimiento preventivo en Inmobiliaria Cocucci</li>
+        <h3>Tareas desarrolladas:</h3>
+        <li>Instalación y mantenimiento preventivo de equipos informáticos (computadoras, servidores, impresoras, scanner, Notebooks, tabletas, etc). </li>
+        <li>Asesoramiento general, instalación de software, documentación y seguimiento de errores. </li>
+        <li> Resolver problemas técnicos tales como reparación de equipos defectuosos. </li>
+        <li>Administración de usuarios, creación y baja de cuentas. </li>
+        <li>Monitorear y supervisión de Backups para resguardar la información. </li>
+        <li>Mantenimiento de riesgos informáticos y medidas de seguridad.</li>
+        <h3>Aptitudes: Aprendizaje contínuo, Soporte al usuario.</h3>
+      </ul>
+
+    <h2>Educacion:</h2>
+
+      <ul>
+        <li>Tecnicatura Superior en Desarrollo de Software.</li>
+        <p>INSTITUTO DE EDUCACIÓN SUPERIOR N° 9-023 CESIT</p>
+        <p>Maipú, Mendoza, Argentina</p>
+        <p>Marzo 2020 - Marzo 2023</p>
+        <p>Cursado finalizado.</p>
+
+        <li>Técnico electrónico con Especialización en Telecomunicaciones.</li>
+        <p>ESCUELA TÉCNICA N° 4-011 CAPITÁN DANIEL MANZOTTI</p>
+        <p>Las heras, Mendoza, Argentina</p>
+        <p>Marzo 2008 - Diciembre 2013</p>
+      </ul>
+
+    <h2>Educacion complementaria:</h2>
+
+      <ul>
+        <li>Master en React: Aprender ReactJS, Hooks, MERN, NodeJS, JWT</li>
+        <li>Javascript desde cero con NodeJS</li>
+        <li>Full Stack Java</li>
+        <li>Scrum Fundamentals Certified</li>
+        <li>Python sin fronteras: HTML, CSS, FLASK Y MySQL</li>
+      </ul>
+
+    <h2>Skills:</h2>
+
+    <ul>
+      <li>HTML</li>
+      <li>CSS3</li>
+      <li>Javascript</li>
+      <li>Typescript</li>
+      <li>NodeJS</li>
+      <li>Express</li>
+      <li>React</li>
+      <li>NestJs</li>
+      <li>MongoDB</li>
+      <li>SQL</li>
+      <li>ORMs</li>
+      <li>Scrum</li>
+      <li>Git, Github</li>
+    </ul>
+
+    <h2>Proyectos personales:</h2>
+    
+    <ul>
+      <li>Proyecto 1</li>
+      <li>Proyecto 2</li>
+    </ul>
+
+  </div>
   )
 }
