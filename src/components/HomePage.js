7fc7c6c67bb1446352549123cf96300bd7200580
Lautaro Decima <decimalautaro@gmail.com>
2023-02-05 16:03:18 -0300

    feat: llenando datos de la pagina principal

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <h1>
+        Hola soy Lautaro Decima, soy desarrollador full stack y ofrezco mis servicios de programación
+         y desarrollo en todo tipo de proyectos web, ya sea back end o front end.
+      </h1>
+      <h2>
+        Alguna de las skills que manejo son: HTML, CSS, Javascript, Typescript, NodeJS, Express, React, NestJs, SQL, MongoDB, ORMs y Métodologias Agiles (Scrum).
+        <Link to="contact">Contacta conmigo.</Link>
+      </h2>
+
+      <section className='lastWorks'>
+        <h2>Algunos de mis proyectos</h2>
+        <p>Estos son algunos de mis proyectos en desarrollo web</p>
+        <div className='works'>
+
+        </div>
+      </section>
+    </div>
   )
 }
