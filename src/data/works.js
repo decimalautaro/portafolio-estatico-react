f6c31f7c2db793a8a4f00a25db1af7000035195c
Lautaro Decima <decimalautaro@gmail.com>
2023-02-06 01:16:58 -0300

    feat:creando objeto de donde accedo a los proyectos

---
+++ src/data/works.js
@@ -0,0 +1,34 @@
+export const works = [
+    {
+        'id': 'crud-nodejs-typescript',
+        'nombre': 'Crud-NodeJS-Typescript',
+        'description': 'Aplicacion web de tipo Server Side Rendering de una empresa de internet usando passport para la autenticacion de usuarios.',
+        'url': 'https://github.com/decimalautaro/Crud-NodeJS-Typescript',
+        'tecnologias': 'Javascript, Typescript, NodeJS, Express, TypeORM',
+        'categories': 'Back end'
+    },
+    {
+        'id': 'cliente-servidor-nodejs',
+        'nombre': 'Cliente-Servidor-NodeJS',
+        'description': 'Consumiendo api de rick and morty: https://rickandmortyapi.com/api y generando archivos csv',
+        'url': 'https://github.com/decimalautaro/cliente-servidor-nodejs',
+        'tecnologias': 'NodeJs, Javascript',
+        'categories': 'Back end'
+    },
+    {
+        'id': 'app-tareas-useState-useEffect',
+        'nombre': 'App-tareas-useState-useEffect',
+        'description': 'Aplicación de tareas usando react y localstorage para almacenar las tareas generadas.',
+        'url': 'https://github.com/decimalautaro/app-tareas-useState-useEffect',
+        'tecnologias': 'Javascript, React,HTML, CSS, BOOTSTRAP',
+        'categories': 'Front end'
+    },
+    {
+        'id': 'maquetacion-web',
+        'nombre': 'Maquetacion-Web',
+        'description': 'Maquetaciones web usando HTML, CSS, Grid, Flex box, Bootstrap y Sass',
+        'url': 'https://github.com/decimalautaro/maquetacion-web',
+        'tecnologias': 'HTML, CSS, Grid, Flex box, BOOTSTRAP',
+        'categories': 'Front end'
+    },
+]
