62643ed7707df96e5dd1993afacb4c0c188972ba
Lautaro Decima <decimalautaro@gmail.com>
2023-02-06 01:18:56 -0300

    fix: corrigiendo errores en rutas

--- src/router/MyRoutes.js
+++ src/router/MyRoutes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Routes, Route, BrowserRouter, Navigate} from "react-router-dom"
 import {HomePage} from "../components/HomePage"
-import {Portafolio} from "../components/Portafolio"
+import {Portfolio} from "../components/Portfolio"
 import {Services} from "../components/Services"
 import {Curriculum} from "../components/Curriculum"
 import {Contact} from "../components/Contact"
@@ -17,12 +17,18 @@ export const MyRoutes = () => {
     {/* CONTENIDO CENTRAL */}
     <section className='content'>
     <Routes>
-        <Route path='/' element= {<Navigate to= "/home"/>}>Inicio</Route>
-        <Route path='/home' element= {<HomePage/>}>Inicio</Route>
-        <Route path='/portafolio' element= {<Portafolio/>}>Portafolio</Route>
-        <Route path='/services' element= {<Services/>}>Servicio</Route>
-        <Route path='/curriculum' element= {<Curriculum/>}>Curriculum</Route>
-        <Route path='/contact' element= {<Contact/>}>Contacto</Route>
+        <Route path='/' element= {<Navigate to= "/home"/>}/>
+        <Route path='/home' element= {<HomePage/>}/>
+        <Route path='/portafolio' element= {<Portfolio/>}/>
+        <Route path='/services' element= {<Services/>}/>
+        <Route path='/curriculum' element= {<Curriculum/>}/>
+        <Route path='/contact' element= {<Contact/>}/>
+        <Route path='*' element= {
+        <div className='page'>
+            <h1 className='heading'>Error 404</h1>
+        </div>
+        }/>
+        
   
     </Routes>
 
